9cf81bb7279b1a8ecf57464bdab3e16e8e6308ce
AbdAlRahman-Odeh-99 <66207782+AbdAlRahman-Odeh-99@users.noreply.github.com>
2021-03-16 21:22:28 +0300

    search y image code

---
+++ searchByImage/LoadTest.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+import base64
+import pickle
+import cv2
+import os
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from PIL import Image
+from numpy import asarray
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+CATEGORIES = []
+basePath = os.path.dirname(os.path.abspath(__file__))
+DATADIR = (f'{os.path.dirname(os.path.abspath(__file__))}/images')
+
+
+def getNameIamge(img, name):
+  image = base64.b64decode(img.img)
+  for folder in os.listdir(DATADIR):
+    CATEGORIES.append(folder)
+  cnn = tf.keras.models.load_model(f'{basePath}/model')
+  path = f'{basePath}/imagesForGettingName'
+  if not os.path.exists(path):
+    os.mkdir(path)
+  path += f'/{name}.png'
+  with open(path, 'wb') as f:
+    f.write(image)
+    img_array = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (70, 70))
+    new_image = new_array.reshape(-1, 70, 70, 1)
+    print(new_image)
+    predictions = cnn.predict(new_image)
+    num = np.argmax(predictions)
+    os.remove(path)
+    if len(CATEGORIES) > num:
+      return CATEGORIES[num]
+    return "Image not found"
---
+++ searchByImage/main.py
@@ -0,0 +1,58 @@
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+import PIL.Image as Image
+from Train import startTrain
+from LoadTest import getNameIamge
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+import uvicorn
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+app = FastAPI(docs_url=None,redoc_url=None)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0')
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8100",
+    "https://b2b-stg.herokuapp.com/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/start-training")
+def read_root():
+  startTrain()
+  return {"finshed": True}
+
+class ImageReader(BaseModel):
+  img: str
+
+@app.post('/get-image-name/{name}')
+def getImageName(name: str, img: ImageReader):
+  print('asddsadsadds')
+  return {"name": getNameIamge(img, name)}
+
+
+@app.post('/add-image/{category_name}/{name}')
+def addImage(category_name: str, name: str, img: ImageReader):
+  directoryName = f'images/{category_name}'
+  if not os.path.exists(directoryName):
+      os.mkdir(directoryName)
+  path = f'{directoryName}/{name}'
+  with open(path, 'wb') as f:
+      f.write(img)
+  return {"finshed": True}
